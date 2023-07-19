4425afae57ba231c103738d63ddb78d886697f9b
William Guimont-Martin <wil9876543210@hotmail.com>
2023-07-18 23:07:51 -0400

    wip

--- src/go.clj
+++ src/go.clj
@@ -1,20 +1,38 @@
 (ns go
   (:require [graph :as g]))
 
-(defn make-board [graph]
-  {:board graph})
+(defn make-board [graph size]
+  {:board graph :size size})
 
-(defn square-board-graph [size]
+(defn update-board [board [i j]]
+  board)
+  ;(if (<= 0 i))
+  ;(-> board
+  ;    (update-board [(dec i) j])
+  ;    (update-board [i (dec j)])
+  ;    (update-board [(inc i) j])
+  ;    (update-board [i (inc j)]))
+
+(defn put-stone [board position color]
+  (let [graph (:board board)
+        board (g/set-node graph position color)
+        board (assoc board :board board)]
+    (update-board board position)))
+
+(defn get-at [board position]
+  (g/get-value (:board board) position))
+
+(defn square-board-board [size]
   (let [graph (atom (g/make-graph))]
     (doseq [i (range size)]
       (doseq [j (range size)]
-          (swap! graph g/set-node [i j] nil)
-          (when (< 0 i)
-            (let [prev-i (dec i)]
-              (swap! graph g/add-edge [i j] [prev-i j])
-              (swap! graph g/add-edge [prev-i j] [i j])))
-          (when (< 0 j)
-            (let [prev-j (dec j)]
-              (swap! graph g/add-edge [i j] [i prev-j])
-              (swap! graph g/add-edge [i prev-j] [i j])))))
-    @graph))
+        (swap! graph g/set-node [i j] nil)
+        (when (< 0 i)
+          (let [prev-i (dec i)]
+            (swap! graph g/add-edge [i j] [prev-i j])
+            (swap! graph g/add-edge [prev-i j] [i j])))
+        (when (< 0 j)
+          (let [prev-j (dec j)]
+            (swap! graph g/add-edge [i j] [i prev-j])
+            (swap! graph g/add-edge [i prev-j] [i j])))))
+    (make-board @graph size)))
--- test/go_test.clj
+++ test/go_test.clj
@@ -1,14 +1,25 @@
 (ns go-test
   (:require [clojure.test :refer :all]
             [clojure.walk :as walk]
-            [graph :as g]
             [go]))
 
 (deftest create-board
-  (let [graph (g/make-graph)
-        board (go/make-board graph)]
-    (is (every? #(contains? board %) []))
-    (is (= graph (:board board)))))
+  (let [board (go/square-board-board 9)]
+    (is (every? #(contains? board %) [:board :size]))
+    (is (= 81 (count (get-in board [:board :values]))))
+    (is (= 9 (:size board)))))
+
+(deftest put-stone
+  (let [board (go/square-board-board 9)
+        board (go/put-stone board [0 0] :white)]
+    (is (= :white (go/get-at board [0 0])))))
+
+(deftest put-stone-update
+  (let [board (-> (go/square-board-board 9)
+                  (go/put-stone [0 0] :white)
+                  (go/put-stone [1 0] :black)
+                  (go/put-stone [0 1] :black))]
+    (is (= nil (go/get-at board [0 0])))))
 
 (defn count-elements [data]
   (walk/prewalk
@@ -19,7 +30,8 @@
     data))
 
 (deftest square-board-graph
-  (let [graph (go/square-board-graph 9)]
+  (let [board (go/square-board-board 9)
+        graph (:board board)]
     (is (= 81 (count (:values graph))))
     (is (= 81 (count (:neighbors graph))))
     (is (every? (fn [[k v]] (<= v 4)) (count-elements (:neighbors graph))))))
