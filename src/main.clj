d1d0e6104e74c33325255599e4aae89254d4cd84
William Guimont-Martin <wil9876543210@hotmail.com>
2023-07-19 14:56:02 -0400

    installed quil

--- src/main.clj
+++ src/main.clj
@@ -1,5 +1,23 @@
-(ns main)
+(ns main
+  (:require [quil.core :as q]))
 
-(defn -main
-  [& args]
-  (println "Hello, World!"))
+(defn setup []
+  (q/frame-rate 1)
+  (q/background 200))
+
+(defn draw []
+  (q/stroke (q/random 255))
+  (q/stroke-weight (q/random 10))
+  (q/fill (q/random 255))
+  (let [diam (q/random 100)
+        x (q/random (q/width))
+        y (q/random (q/height))]
+    (q/ellipse x y diam diam)))
+
+(defn -main []
+  (q/defsketch example
+               :title "Oh so many grey circles"
+               :settings #(q/smooth 2)
+               :setup setup
+               :draw draw
+               :size [1280 720]))
