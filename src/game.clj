f84ec4b96ef18ee8b803a1c31aecbb2e1c16d803
William Guimont-Martin <wil9876543210@hotmail.com>
2023-07-20 12:51:00 -0400

    ko rule

--- src/game.clj
+++ src/game.clj
@@ -2,7 +2,7 @@
   (:require [go :as g]))
 
 (defn make-game [board]
-  {:board board :turn :black :num-pass 0})
+  {:board board :turn :black :num-pass 0 :history []})
 
 (defn next-turn [turn]
   (cond
@@ -11,11 +11,16 @@
 
 (defn play-turn [game position]
   (let [board (:board game)
-        board (g/put-stone board position (:turn game))
-        game (assoc game :board board)]
-    (some-> game
-            (update :turn next-turn)
-            (assoc :num-pass 0))))
+        board-new (g/put-stone board position (:turn game))
+        game-new (assoc game :board board-new)]
+    (if (or (= nil board-new) (= board-new (last (:history game))))
+      game
+      (some-> game-new
+              (update :turn next-turn)
+              (assoc :num-pass 0)
+              (update :history conj board)))))
 
 (defn pass [game]
-  (update game :num-pass inc))
+  (-> game
+    (update :num-pass inc)
+    (update :history conj (:board game))))
--- test/game_test.clj
+++ test/game_test.clj
@@ -1,8 +1,7 @@
 (ns game-test
   (:require [clojure.test :refer :all]
-            [graph :as graph]
-            [go :as go]
-            [game :as g]))
+            [game :as g]
+            [go :as go]))
 
 (deftest make-game
   (let [board (go/make-square-board 9)
@@ -18,16 +17,39 @@
 
 (deftest play-turn
   (let [game (g/make-game (go/make-square-board 9))
-        game (g/pass game)
-        game (g/play-turn game [0 0])
-        board (:board game)]
-    (is (= (:num-pass game) 0))
-    (is (= (:turn game) :white))
-    (is (= (go/get-at board [0 0]) :black))))
+        game-1 (g/pass game)
+        game-2 (g/play-turn game-1 [0 0])
+        game-3 (g/play-turn game-2 [0 1])
+        game-4 (g/play-turn game-3 [0 2])
+        board (:board game-4)]
+    (is (= (:num-pass game-4) 0))
+    (is (= (:turn game-4) :white))
+    (is (= (go/get-at board [0 0]) :black))
+    (is (= (last (:history game-4)) (:board game-3)))))
+
+(deftest play-turn-ko
+  (let [game (-> (g/make-game (go/make-square-board 9))
+                 (g/play-turn [0 1])
+                 (g/play-turn [3 1])
+                 (g/play-turn [1 0])
+                 (g/play-turn [2 2])
+                 (g/play-turn [1 2])
+                 (g/play-turn [2 0])
+                 (g/play-turn [2 1])
+                 (g/play-turn [1 1]))]
+    (is (= (go/get-at (:board game) [2 1]) nil))
+    (is (= (g/play-turn game [2 1]) game))))
+
+(deftest play-turn-bad-move
+  (let [game (g/make-game (go/make-square-board 9))
+        game-1 (g/play-turn game [0 0])
+        game-2 (g/play-turn game-1 [0 0])]
+    (is (= game-2 game-1))))
 
 (deftest pass
   (let [game (g/make-game (go/make-square-board 9))
         game-1 (g/pass game)
         game-2 (g/pass game-1)]
     (is (:num-pass game-1) 1)
-    (is (:num-pass game-2) 2)))
+    (is (:num-pass game-2) 2)
+    (is (= (last (:history game-2)) (:board game-1)))))
